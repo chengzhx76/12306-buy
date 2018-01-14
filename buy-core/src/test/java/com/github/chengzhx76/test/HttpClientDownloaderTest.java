2f741a1795e97c3bb30f63feaf69a5121fb261d1
chengzhx76 <chengzhx76@qq.com>
2018-01-14 23:37:33 +0800

    mod

--- buy-core/src/test/java/com/github/chengzhx76/test/HttpClientDownloaderTest.java
+++ buy-core/src/test/java/com/github/chengzhx76/test/HttpClientDownloaderTest.java
@@ -1,19 +1,23 @@
 package com.github.chengzhx76.test;
 
-import com.github.chengzhx76.buy.model.Request;
-import com.github.chengzhx76.buy.model.Response;
 import com.github.chengzhx76.buy.Site;
 import com.github.chengzhx76.buy.httper.HttpClientDownloader;
+import com.github.chengzhx76.buy.httper.HttpUriRequestConverter;
+import com.github.chengzhx76.buy.model.Request;
+import com.github.chengzhx76.buy.model.Response;
 import com.github.chengzhx76.buy.utils.HttpConstant;
 import com.github.chengzhx76.buy.utils.OperationType;
 import com.github.dreamhead.moco.HttpServer;
 import com.github.dreamhead.moco.Runnable;
 import com.github.dreamhead.moco.Runner;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static com.github.dreamhead.moco.Moco.*;
-import static com.github.dreamhead.moco.Moco.cookie;
-import static com.github.dreamhead.moco.Moco.eq;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -78,10 +82,87 @@ public class HttpClientDownloaderTest {
                 request.setOperation(OperationType.TEST);
                 request.addCookie("cookie","cookie-cheng");
                 Response response = httpClientDownloader.request(request, Site.me().setDisableCookieManagement(true));
-                assertThat(response.getContent()).isEqualTo("ok");
+                assertThat(response.getRawText()).isEqualTo("ok");
             }
         });
     }
 
+    @Test
+    public void test_selectRequestMethod() throws Exception {
+        HttpServer server = httpServer(9091);
+        server.post(eq(query("q"), "webmagic")).response("post");
+        Runner.running(server, new Runnable() {
+            @Override
+            public void run() throws Exception {
+                /*HttpClientDownloader httpClientDownloader = new HttpClientDownloader();
+                Request request = new Request();
+                Map<String, Object> params = new HashMap<>();
+                params.put("q","webmagic");
+
+                request.setUrl("http://127.0.0.1:9091/search");
+                request.setMethod(HttpConstant.Method.POST);
+                request.setRequestBody(HttpRequestBody.form(params, "utf-8"));
+
+                Response response =httpClientDownloader.request(request, Site.me());
+                System.out.println(response.getRawText());
+                assertThat(response.getRawText()).isEqualTo("post");*/
+
+                //HttpUriRequest httpUriRequest = httpUriRequestConverter.convert(request, Site.me(), null).getHttpUriRequest();
+                //assertThat(EntityUtils.toString(HttpClients.custom().build().execute(httpUriRequest).getEntity())).isEqualTo("post");
+
+                //String data = org.apache.http.client.fluent.Request.Post("https://kyfw.12306.cn/passport/captcha/captcha-check")
+                String data = org.apache.http.client.fluent.Request.Post("https://kyfw.12306.cn/otn/login/checkUser")
+                        .execute().returnContent().asString();
+
+                System.out.println(data);
+
+            }
+        });
+
+    }
+
+    @Test
+    public void test_postMethod() throws IOException {
+        String checkUser = "https://kyfw.12306.cn/otn/login/checkUser";
+        //String checkUser = "http://localhost/ServletMVC/index?method=index";
+        String data = "";
+        //data = org.apache.http.client.fluent.Request.Post("https://kyfw.12306.cn/otn/login/checkUser")
+        //        .execute().returnContent().asString();
+
+        Request request = new Request();
+        request.setUrl(checkUser);
+        request.setMethod(HttpConstant.Method.POST);
+        //Map<String, Object> params = new HashMap<>();
+        //params.put("q","webmagic");
+        //request.setRequestBody(HttpRequestBody.form(params, "utf-8"));
+
+        /*HttpClientDownloader httpClientDownloader = new HttpClientDownloader();
+        Response response =httpClientDownloader.request(request, Site.me());
+        data = response.getRawText();*/
+
+        HttpUriRequestConverter httpUriRequestConverter = new HttpUriRequestConverter();
+        HttpUriRequest httpUriRequest = httpUriRequestConverter.convert(request, Site.me(), null).getHttpUriRequest();
+        //HttpUriRequest httpUriRequest = new HttpGet(checkUser);
+        data = EntityUtils.toString(HttpClients.custom().build().execute(httpUriRequest).getEntity(), "UTF-8");
+
+        System.out.println(data);
+    }
+
+    @Test
+    public void test_postMethod_2() throws IOException {
+        String checkUser = "https://kyfw.12306.cn/otn/login/checkUser";
+        String data = "";
+        //data = org.apache.http.client.fluent.Request.Post("https://kyfw.12306.cn/otn/login/checkUser")
+        //        .execute().returnContent().asString();
+
+        Request request = new Request();
+        request.setUrl(checkUser);
+        request.setMethod(HttpConstant.Method.POST);
+        HttpUriRequestConverter httpUriRequestConverter = new HttpUriRequestConverter();
+        HttpUriRequest httpUriRequest = httpUriRequestConverter.convert(request, Site.me(), null).getHttpUriRequest();
+        data = EntityUtils.toString(HttpClients.custom().build().execute(httpUriRequest).getEntity(), "UTF-8");
+
+        System.out.println(data);
+    }
 
 }
