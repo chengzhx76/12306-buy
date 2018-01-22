9a7b4434ce626ab2bd95a8527c85b1babd29b653
chengzhx76 <chengzhx76@qq.com>
2018-01-22 22:47:58 +0800

    mod

--- buy-core/src/test/java/com/github/chengzhx76/test/JsonTest.java
+++ buy-core/src/test/java/com/github/chengzhx76/test/JsonTest.java
@@ -1,8 +1,10 @@
 package com.github.chengzhx76.test;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.github.chengzhx76.buy.model.Query;
 import org.apache.commons.io.IOUtils;
+import org.junit.Test;
 
 import java.io.IOException;
 
@@ -17,6 +19,15 @@ public class JsonTest {
         String data = IOUtils.toString(JsonTest.class.getClassLoader().getResourceAsStream("data.json"));
         Query query = JSON.parseObject(data, Query.class);
         System.out.println(query);
+    }
+
+    @Test
+    public void test_jsonObject() throws IOException {
+        String data = IOUtils.toString(JsonTest.class.getClassLoader().getResourceAsStream("data.json"));
+        System.out.println(data);
+        JSONObject object = JSON.parseObject(data);
+        System.out.println(object.getString("cardTypes"));
+        System.out.println(object.getJSONArray("cardTypes").get(0));
     }
 
 }
